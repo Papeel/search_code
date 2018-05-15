5c0d548d91560f304ca40589a94f5cc77c66b973
Nelson <nelsongonmac@gmail.com>
2018-05-15 17:18:22 +0100

    1.0

---
+++ run.py
@@ -0,0 +1,31 @@
+# Search methods
+
+import search
+
+ab = search.GPSProblem('A', 'B', search.romania)
+
+
+print( search.breadth_first_graph_search(ab).path())
+print (search.depth_first_graph_search(ab).path())
+
+print (search.acota(ab).path())
+print (search.acotaheu(ab).path())
+
+ab = search.GPSProblem('T', 'N', search.romania)
+
+print (search.acota(ab).path())
+print (search.acotaheu(ab).path())
+
+
+ab = search.GPSProblem('Z', 'S', search.romania)
+
+print (search.acota(ab).path())
+print (search.acotaheu(ab).path())
+#print search.iterative_deepening_search(ab).path()
+#print search.depth_limited_search(ab).path()
+
+#print search.astar_search(ab).path()
+
+# Result:
+# [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
+# [<Node B>, <Node F>, <Node S>, <Node A>] : 211 + 99 + 140 = 450
